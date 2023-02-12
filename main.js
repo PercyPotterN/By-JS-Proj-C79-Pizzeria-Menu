65ff51d9744e7f1082b684828ecebe4b677fb2ee
PercyPotterN <111822433+PercyPotterN@users.noreply.github.com>
2023-02-12 10:23:23 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+// /Users/n4m1/Downloads/Byjus_Nihal_Projects/202302-Project-79/main.js
+
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item =   document.getElementById("add_item").value;
+        menu_list_array.sort();        // use the sort function as - menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    //Cannot enter empty suggestion or duplicate suggestion
+    if (item == "") {
+        
+    }
+    else if (1==2) //|| menu_list_array.EXIST (item) 
+    {
+        // Check if duplicate then prompt user it is already added
+        // NEED HELP FROM TEACHER
+    }
+    else {
+        menu_list_array.push(item);
+	    add_item();
+    }
+}
